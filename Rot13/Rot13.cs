5f2df3ce89d82f8107eada5fbf1a40da5094b425
Max Gorankov <maksim.gorankov@gmail.com>
2020-04-14 20:56:40 +0500

    First try of Rot13

--- Rot13/Rot13.cs
+++ Rot13/Rot13.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Linq;
 
 namespace Rot13
 {
     public class Rot13
     {
-        public string Encode(string word)
-        {
-            return default;
-        }
+        public string Encode(string word)      
+         => new string(word.ToCharArray().Select(x => Convert.ToInt32(x)).Select(x =>
+                                                   ((Char.IsLetter(Char.ConvertFromUtf32(x).ToCharArray()[0])) ?
+                                                   ((x >= 97) ?
+                                                   ((x + 13 > 122) ? 96 + (x + 13 - 122) : x + 13) :
+                                                   ((x + 13 > 90) ? 64 + (x + 13 - 90) : x + 13))
+                                                   : x))
+                                                   .Select(x => Char.ConvertFromUtf32(x).ToCharArray()[0]).ToArray());
+
     }
 }
--- Rot13Tests/Rot13Tests.cs
+++ Rot13Tests/Rot13Tests.cs
@@ -20,7 +20,21 @@ namespace Rot13.Tests
         public void Test_To_Grfg()
         {
             Rot13 encoder = new Rot13();
-            Assert.AreEqual("grfg", encoder.Encode("test"));
+            Assert.AreEqual("Grfg", encoder.Encode("Test"));
+        }
+
+        [Test]
+        public void TEST_To_GRFG()
+        {
+            Rot13 encoder = new Rot13();
+            Assert.AreEqual("GRFG", encoder.Encode("TEST"));
+        }
+
+        [Test]
+        public void test10plus2_is_twelve()
+        {
+            Rot13 encoder = new Rot13();
+            Assert.AreEqual("10+2 is twelve.", encoder.Encode("10+2 vf gjryir."));
         }
     }
 }
