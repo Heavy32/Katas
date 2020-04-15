00518ab4ffa4599015e3ca459a688d1618d2046e
Max Gorankov <maksim.gorankov@gmail.com>
2020-04-15 14:48:50 +0500

    Perfect decision (not mine)

--- Rot13/Rot13.cs
+++ Rot13/Rot13.cs
@@ -5,14 +5,7 @@ namespace Rot13
 {
     public class Rot13
     {
-        public string Encode(string word)      
-         => new string(word.ToCharArray().Select(x => Convert.ToInt32(x)).Select(x =>
-                                                   ((Char.IsLetter(Char.ConvertFromUtf32(x).ToCharArray()[0])) ?
-                                                   ((x >= 97) ?
-                                                   ((x + 13 > 122) ? 96 + (x + 13 - 122) : x + 13) :
-                                                   ((x + 13 > 90) ? 64 + (x + 13 - 90) : x + 13))
-                                                   : x))
-                                                   .Select(x => Char.ConvertFromUtf32(x).ToCharArray()[0]).ToArray());
-
+        public string Encode(string word)
+         => new string(word.Select(x => char.IsLetter(x) ? (x >= 65 && x <= 77) || (x >= 97 && x <= 109) ? (char)(x + 13) : (char)(x - 13) : x).ToArray());
     }
 }
