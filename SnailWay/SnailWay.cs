1f760b816d441a2495212b62fcd32f8d3cae02d8
Max Gorankov <maksim.gorankov@gmail.com>
2020-04-22 14:01:40 +0500

    Add snail katas with tests

---
+++ SnailWay/SnailWay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SnailWay
+{
+    public class SnailWay
+    {
+        public static int[] CalculateWay(int[][] grid, List<int> snailWay)
+        {
+            snailWay.AddRange(grid[0]);
+            grid = grid.Skip(1).ToArray();
+
+            snailWay.AddRange(grid.Where((item, index) => index != grid.Length - 1).Select(x => x.Last()).ToArray());
+            grid = Enumerable.Range(0, grid.Length).Select(x => (x == grid.Length - 1) ? grid[x] : grid[x].Where((number, index) => index != grid[x].Length - 1).ToArray()).ToArray();
+
+            Array.Reverse(grid);
+            grid = grid.Select(x => x.Reverse().ToArray()).ToArray();
+
+            return (grid.Length == 0) ? snailWay.ToArray() : CalculateWay(grid, snailWay);
+        }
+    }
+}
---
+++ SnailWayTests/SnailWayTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace SnailWay.Tests
+{
+    [TestFixture]
+    public class SnailWayTests
+    {
+        [Test]
+        public void Array4x4()
+        {
+            int[][] grid =
+            {
+                new []{ 1, 2, 3, 9 },
+                new []{ 4, 5, 6, 4 },
+                new []{ 7, 8, 9, 1 },
+                new []{ 1, 2, 3, 4 },
+            };
+
+            Assert.AreEqual(new int[] { 1, 2, 3, 9, 4, 1, 4, 3, 2, 1, 7, 4, 5, 6, 9, 8}, SnailWay.CalculateWay(grid, new List<int>()));
+        }
+
+        [Test]
+        public void Array3x3()
+        {
+            int[][] grid =
+            {
+                new []{ 1, 2, 3},
+                new []{ 8, 9, 4},
+                new []{ 7, 6, 5},
+            };
+
+            Assert.AreEqual(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9}, SnailWay.CalculateWay(grid, new List<int>()));
+        }
+    }
+}
