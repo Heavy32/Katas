dbf474fa7446f4c6ae7325b8b6deae244fec456e
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-10 14:47:14 +0500

    Add first version

---
+++ StripComments/StripComment.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace StripComments
+{
+    class StripComment
+    {
+        public static string StripComments(string text, string[] commentSymbols)
+        {
+            var lines = text.Split('\n');
+
+            for (int i = 0; i < commentSymbols.Length; i++)
+            {
+                lines = lines.Select(x => x.Substring(0, (x.Contains(commentSymbols[i]) ? x.IndexOf(commentSymbols[i]) : x.Length)).TrimEnd()).ToArray();
+            }
+
+            return string.Join('\n', lines);
+        }
+    }
+}
