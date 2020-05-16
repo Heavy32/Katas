c574e33ea88190060e9d2c52cd3d8baee3015c89
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-16 21:59:20 +0500

    Add first version

---
+++ NextBiggerNumber/NextBiggerNumber.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NextBiggerNumber
+{
+    public class NextBiggerNumber
+    {
+        public static long Find(long n)
+        {
+            List<int> digitsFirstNumber = NumberToDigitsList(n);
+            var tempList = digitsFirstNumber;
+            tempList.Sort();
+            tempList.Reverse();
+            long maxValue = IntListToInt(tempList);
+            if(maxValue == n) { return -1; }
+
+            long firstNumber = n;
+            while (true)
+            {
+                firstNumber += 1;
+                List<int> digitsNewNumber = NumberToDigitsList(firstNumber);
+                digitsFirstNumber.Sort();
+                digitsNewNumber.Sort();
+                if (Enumerable.SequenceEqual(digitsNewNumber, digitsFirstNumber))
+                {
+                    return firstNumber;
+                }
+            }
+        }
+
+        private static long IntListToInt(List<int> digits)
+        {
+            long number = 0;
+            for (int i = 0; i < digits.Count; i++)
+            {
+                number += (long)(digits[i] * (Math.Pow(10, digits.Count - i - 1)));
+            }
+            return number;
+        }
+
+        private static List<int> SwapElementsInList(int IDfirst, int IDsecond, List<int> digits)
+        {
+            int temp = digits[IDfirst];
+            digits[IDfirst] = digits[IDsecond];
+            digits[IDsecond] = temp;
+
+            return digits;
+        }
+
+        private static List<int> NumberToDigitsList(long n)
+        {
+            List<int> digits = new List<int>();
+
+            while (n != 0)
+            {
+                digits.Add((int)(n % 10));
+                n /= 10;
+            }
+
+            digits.Reverse();
+
+            return digits;
+        }
+    }
+}
---
+++ NextBiggerNumberTests/NextBiggerNumberTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+
+namespace NextBiggerNumber.Tests
+{
+    [TestFixture]
+    public class NextBiggerNumberTests
+    {
+        [Test]
+        public void In123_Out132()
+        {
+            Assert.AreEqual(132, NextBiggerNumber.Find(123));
+        }
+
+        [Test]
+        public void In698_Out869()
+        {
+            Assert.AreEqual(869, NextBiggerNumber.Find(698));
+        }
+
+        [Test]
+        public void In144_Out414()
+        {
+            Assert.AreEqual(414, NextBiggerNumber.Find(144));
+        }
+
+        [Test]
+        public void In2017_Out2071()
+        {
+            Assert.AreEqual(2071, NextBiggerNumber.Find(2017));
+        }
+
+        [Test]
+        public void In2071_Out2107()
+        {
+            Assert.AreEqual(2107, NextBiggerNumber.Find(2071));
+        }
+
+        [Test]
+        public void In227801797_Out227801977()
+        {
+            Assert.AreEqual(227801977, NextBiggerNumber.Find(227801797));
+        }
+
+        [Test]
+        public void In1797_Out1977()
+        {
+            Assert.AreEqual(1977, NextBiggerNumber.Find(1797));
+        }
+
+        [Test]
+        public void In531_OutMinus1()
+        {
+            Assert.AreEqual(-1, NextBiggerNumber.Find(531));
+        }
+
+        [Test]
+        public void In9887654332_OutMinus1()
+        {
+            Assert.AreEqual(-1, NextBiggerNumber.Find(9887654332));
+        }
+    }
+}
